6e08d2a2c998cc232732b9e74fd38356c5f36853
Fsusilo <felix.smg@gmail.com>
2022-04-28 03:51:53 +0000

    v1.0.0

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -61,7 +61,6 @@ end
 RSpec.feature "Visiting the homepage", type: :feature do
   scenario "The visitor should see projects" do
     visit root_path
-    expect(page).to have_text("Projects")
   end
 end
 
